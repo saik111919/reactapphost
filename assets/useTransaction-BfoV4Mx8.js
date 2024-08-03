05bd7c6e6af34ffaa299597ff70fb85afaaecbb9
saik111919 <111496874+saik111919@users.noreply.github.com>
2024-08-04 01:13:28 +0530

    Updates

---
+++ assets/useTransaction-BfoV4Mx8.js
@@ -0,0 +1,1 @@
+import{r as e,f,G as i}from"./index-B4Md8A4A.js";const u=()=>{const[o,r]=e.useState([]),[a,c]=f(!1),t=e.useCallback(async()=>{a(!0);try{const s=await i(),{data:n}=s;r(n)}catch(s){console.error("Failed to fetch transactions:",s)}finally{a(!1)}},[a]);return e.useEffect(()=>{t()},[t]),{data:o,LoaderComp:c,setLoader:a,fetchTransactions:t}};export{u};
