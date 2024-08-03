be9927b8d8489c41e148b622a36654dedc5e5d2b
saik111919 <111496874+saik111919@users.noreply.github.com>
2024-08-04 00:11:57 +0530

    Updates

--- assets/useTransaction-DhPWRkSP.js
+++
@@ -1,1 +0,0 @@
-import{r as e,f,g as i}from"./index-Bb9cCbD0.js";const u=()=>{const[o,r]=e.useState([]),[a,c]=f(!1),t=e.useCallback(async()=>{a(!0);try{const s=await i(),{data:n}=s;r(n)}catch(s){console.error("Failed to fetch transactions:",s)}finally{a(!1)}},[a]);return e.useEffect(()=>{t()},[t]),{data:o,LoaderComp:c,setLoader:a,fetchTransactions:t}};export{u};
---
+++ assets/useTransaction-G77u4Ddx.js
@@ -0,0 +1,1 @@
+import{r as e,f,g as i}from"./index-BCoCwZYC.js";const u=()=>{const[o,r]=e.useState([]),[a,c]=f(!1),t=e.useCallback(async()=>{a(!0);try{const s=await i(),{data:n}=s;r(n)}catch(s){console.error("Failed to fetch transactions:",s)}finally{a(!1)}},[a]);return e.useEffect(()=>{t()},[t]),{data:o,LoaderComp:c,setLoader:a,fetchTransactions:t}};export{u};
